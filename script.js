d6511bbe5ea0f854573a287bbd9cc460204948d5
tirilkoder <tiril.koder@gmail.com>
2023-10-30 12:49:59 +0100

    Design and script

---
+++ script.js
@@ -0,0 +1,7 @@
+// må lage klassenavn og id til elementa
+import { themeToggle } from "./themeToggle.js";
+
+const button = document.querySelector("button");
+
+
+button.addEventListener("click", themeToggle);
---
+++ themeToggle.js
@@ -0,0 +1,5 @@
+export function themeToggle() {
+    const body = document.querySelector(".body");
+    body.classList.toggle("dark_mode");
+    console.log("button clicked");
+}    
