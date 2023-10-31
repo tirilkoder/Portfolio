1b488e16ce688687f2b5db23b23e410cd80511af
tirilkoder <tiril.koder@gmail.com>
2023-10-31 14:42:49 +0100

    Dark mode

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// må lage klassenavn og id til elementa
-import { themeToggle } from "./themeToggle.js";
+// imported function "themeToggle" from a module in the "theme_toggle.js" file
+import { themeToggle } from "./theme_toggle.js";
 
-const button = document.querySelector("button");
+// Find and store a reference to the class "toggle_button" in the document
+const button = document.querySelector(".toggle_button");
 
-
+// Add an event listener to the div class, listening for a "click" event, and preforming the function when activated
 button.addEventListener("click", themeToggle);
--- themeToggle.js
+++
@@ -1,5 +0,0 @@
-export function themeToggle() {
-    const body = document.querySelector(".body");
-    body.classList.toggle("dark_mode");
-    console.log("button clicked");
-}    
